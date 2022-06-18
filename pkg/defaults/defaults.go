d5115dd54f299a0d13e80b1bbb1ec76d3aa8c98f
Petr Fedchenkov <giggsoff@gmail.com>
2022-06-18 17:19:04 +0300

    no rdtscp
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -52,7 +52,7 @@ const (
 	DefaultRegistryPort         = 5050
 
 	//tags, versions, repos
-	DefaultEVETag               = "0.0.0-no-clocksource-aef66e01" // DefaultEVETag tag for EVE image
+	DefaultEVETag               = "0.0.0-fixed-vhost-b989e2f8" // DefaultEVETag tag for EVE image
 	DefaultAdamTag              = "0.0.39"
 	DefaultRedisTag             = "6"
 	DefaultRegistryTag          = "2.7"
@@ -115,7 +115,7 @@ const (
 
 	DefaultQemuAccelDarwin     = "-machine q35,accel=hvf -cpu kvm64,kvmclock=off "
 	DefaultQemuAccelLinuxAmd64 = "-machine q35,accel=kvm,dump-guest-core=off,kernel-irqchip=split -cpu host,invtsc=on,kvmclock=off -device intel-iommu,intremap=on,caching-mode=on,aw-bits=48 "
-	DefaultQemulAmd64          = "-machine q35 --cpu SandyBridge "
+	DefaultQemulAmd64          = "-machine q35 --cpu SandyBridge,rdtscp=off "
 
 	DefaultQemuAccelArm64 = "-machine virt,accel=kvm,usb=off,dump-guest-core=off -cpu host "
 	DefaultQemulArm64     = "-machine virt,virtualization=true -cpu cortex-a57 "

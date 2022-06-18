6aace9345a83b34d3dea80b97a2af816af987ee0
Petr Fedchenkov <giggsoff@gmail.com>
2022-06-18 13:50:33 +0300

    no clocksource
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -52,7 +52,7 @@ const (
 	DefaultRegistryPort         = 5050
 
 	//tags, versions, repos
-	DefaultEVETag               = "0.0.0-hpet-83fa2a77" // DefaultEVETag tag for EVE image
+	DefaultEVETag               = "0.0.0-no-clocksource-aef66e01" // DefaultEVETag tag for EVE image
 	DefaultAdamTag              = "0.0.39"
 	DefaultRedisTag             = "6"
 	DefaultRegistryTag          = "2.7"

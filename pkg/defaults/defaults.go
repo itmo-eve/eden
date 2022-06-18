0aa3d71441d907a31c5734d27279e10c45b42925
Petr Fedchenkov <giggsoff@gmail.com>
2022-06-18 20:45:47 +0300

    qemu 7.0.0 + hpet
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -52,7 +52,7 @@ const (
 	DefaultRegistryPort         = 5050
 
 	//tags, versions, repos
-	DefaultEVETag               = "0.0.0-fixed-vhost-b989e2f8" // DefaultEVETag tag for EVE image
+	DefaultEVETag               = "0.0.0-hpet-83fa2a77" // DefaultEVETag tag for EVE image
 	DefaultAdamTag              = "0.0.39"
 	DefaultRedisTag             = "6"
 	DefaultRegistryTag          = "2.7"

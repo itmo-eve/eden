f0bf8e0014d4b6aa2725b9f940389a1432a4bf0f
Petr Fedchenkov <giggsoff@gmail.com>
2022-12-16 12:17:48 +0300

    Disable KeepAlive
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- tests/eclient/image/files/root/portmap_test.sh
+++ tests/eclient/image/files/root/portmap_test.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-echo ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@"$1" -p "$2" grep Ubuntu /etc/issue
-ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@"$1" -p "$2" grep Ubuntu /etc/issue
+echo ssh -o ConnectTimeout=10 -o KeepAlive=no -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@"$1" -p "$2" grep Ubuntu /etc/issue
+ssh -o ConnectTimeout=10 -o KeepAlive=no -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@"$1" -p "$2" grep Ubuntu /etc/issue
